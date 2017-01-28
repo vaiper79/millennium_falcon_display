d6a978cd504a6e4846f657439b2fa1071324f665
Ole Andre <olestordal@gmail.com>
2017-01-28 16:45:46 +0100

    Found and squashed a "bug", I forgot to set audio back to 0 after the takeOff sequence. If you did a allReset and tried to take off again, the Millennium Falcon would be grounded. Now it flies, every time! Also mopped up the allReset function. Think I will keep it around, nice to use it to turn off. Did however rearrange when things were executed. Put the only item with a 1 second delay at the end. Could use millis and avoid it, but for simplicity I did it this way. Also added commands to initiate any of the audio pins, this is just for testing during development.

--- millenniumfalcon_v2.ino
+++ millenniumfalcon_v2.ino
@@ -1,18 +1,13 @@
+
+
 // Get this show started, we will use Neopixels in this version
 #include "application.h"        // Not 100% sure what this is..
 #include "neopixel/neopixel.h"  // Neopixel Library
 
 // Set Photon to correct mode
-SYSTEM_MODE(AUTOMATIC);
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
 // Pin Definitions
-// LED Strips
-#define pixelPin    DAC // Plan tao use juse a long LED strip, and assign a certain portion to the engine, cockpit etc. 
-
-// Misc pins
-#define relPin      WKP // Controlling the relay
-#define irSensor    RX  // For when I get there.. 
-
 // Audio Triggers
 #define audioPin00  D6  // Left Right Left Right
 #define audioPin01  D5  // Some random Falcon sound
@@ -22,9 +17,16 @@ SYSTEM_MODE(AUTOMATIC);
 #define audioPin05  D1  //
 #define audioPin06  D0  //
 #define audioPin07  A3  //
-#define audioPin08  A2  //
+#define audioPin08  A2  //  
 #define audioPin09  A1  //
 #define audioPin10  A0  //
+
+// LED Strip
+#define pixelPin    DAC // Plan tao use juse a long LED strip, and assign a certain portion to the engine, cockpit etc. 
+
+// Misc pins
+#define relPin      WKP // Controlling the relay
+#define irSensor    RX  // For when I get there.. 
 
 // Volume - issues..looking at other option
 #define volumeUp    A5
@@ -43,9 +45,11 @@ SYSTEM_MODE(AUTOMATIC);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numPix, pixelPin, pixType);
 
 String prevCmd;                 // Right..gotta set the previously used pins low again..
-int looper = 999;               // So we don't enter any if statements in the void loop :)
+int wait = 1000;
+int looper = 0;               // So we don't enter any if statements in the void loop :)
 int audio = 0;                  // Used to set state so audio files are only triggered once
 int ledState = 1;
+String lowPin;
 unsigned long startTime = 0;    // For timing lights and sound
 unsigned long sequenceStart;    // For timing lights and sound
 
@@ -55,11 +59,12 @@ unsigned long interval = 1000;
 
 
 void setup(){
-    Serial.begin(9600);                     // Start serial interface, in case we need some output for debuggig.
-    
     Particle.function("falcon", falcon);    // The cloud exposed function
     
     pinMode(pixelPin, OUTPUT);              // Defining the neopixel pin as output
+   
+    pinMode(audioRst, OUTPUT);              // Defining the audio reset pin as output
+    digitalWrite(audioRst, HIGH);           // Setting it high so as to not trigger, yet.. :) 
    
     pinMode(audioPin00, OUTPUT);            // Pins defined as outputs
     digitalWrite(audioPin00, HIGH);         // The set high due to the nature of the audio board
@@ -89,9 +94,6 @@ void setup(){
     pinMode(volumeDown, OUTPUT);
     digitalWrite(volumeDown, HIGH);
     
-    pinMode(audioRst, OUTPUT);              // Defining the audio reset pin as output
-    digitalWrite(audioRst, HIGH);           // Setting it high so as to not trigger, yet.. :) 
-    
     pinMode(relPin, OUTPUT);
     digitalWrite(relPin, LOW);
     
@@ -101,41 +103,45 @@ void setup(){
 
 int falcon(String cmd) {
     
-    if (cmd == "relayOff"){
-        relay("off");
-    }
-    if (cmd == "relayOn"){
-        relay("on");
-    }
-    if (cmd == "off") {         // All Stop..mostly meant for use during development
-        looper = 0;
-        resetAudio();           // Stop sound
-        relay("off");           // Stop power to amp
-        lightsOff();            // Lights out!
-        pinFixer();
-    }    
-    if (cmd == "onBasic") {     // This will become the default when switched on. Enigne on, no sound.
-        relay("off");             
-        looper = 1;
-    }
-    if (cmd == "onAdvanced"){   // This will become the default when switched on. Enigne on, some random sounds
-        relay("on");
-        looper = 2;
-    }
-    if (cmd == "takeOff"){      // One of the special sequences requiring timing to work as indended
-        prevCmd = cmd;
-        relay("on");
-        looper = 3;
-        sequenceStart = millis();
-    }
-    if (cmd == "hyperdrive"){   // One of the special sequences requiring timing to work as indended 
-        looper = 4;
-    }
+    // Management Commands
+    if (cmd == "relayOn") relay("on");
+    if (cmd == "relayOff") relay("off");
+    if (cmd == "reset") resetAudio();
+    if (cmd == "lightsOff") lightsOff();
     if (cmd == "up"){           // Volume UP by 5
         volumeU(5);
     }
     if (cmd == "down"){         // Volume DOWN by 5
         volumeD(5);
+    }
+    if (cmd == "allReset") {         // All Stop..mostly meant for use during development
+        looper = 0;
+        pinFixer(lowPin);
+        lightsOff();            // Lights out!
+        relay("off");           // Stop power to amp
+        resetAudio();           // Stop sound
+    }    
+    
+    // Display Commands
+    if (cmd == "onBasic") {     // This will become the default when switched on. Enigne on, no sound.
+        relay("off");             
+        onBasic();
+    }
+    if (cmd == "onAdvanced"){   // This will become the default when switched on. Enigne on, some random sounds
+        relay("on");
+        onAdvanced();
+    }
+    if (cmd == "takeOff"){      // One of the special sequences requiring timing to work as indended
+        audio = 0;
+        prevCmd = cmd;
+        //cmd = "";
+        relay("on");
+        looper = 3;
+        sequenceStart = millis(); 
+        pinFixer(lowPin);
+    }
+    if (cmd == "hyperdrive"){   // One of the special sequences requiring timing to work as indended 
+        looper = 4;
     }
     if (cmd == "annoying"){     // Silly test sequence..for development only 
         prevCmd = cmd;
@@ -143,30 +149,95 @@ int falcon(String cmd) {
         looper = 5;
         sequenceStart = millis();
     }
+    
+    //Test    
+    if (cmd == "00") { 
+        digitalWrite(audioPin00, LOW);
+        lowPin = "00";
+        delay(wait);
+        digitalWrite(audioPin00, HIGH);
+    } 
+    if (cmd == "01") {   
+        digitalWrite(audioPin01, LOW);
+        lowPin = "01";
+        delay(wait);
+        digitalWrite(audioPin01, HIGH);
+    }    
+    if (cmd == "02") {        
+        digitalWrite(audioPin02, LOW);
+        lowPin = "02";
+        delay(wait);
+        digitalWrite(audioPin02, HIGH);
+    }    
+    if (cmd == "03") {      
+        digitalWrite(audioPin03, LOW);
+        lowPin = "03";
+        delay(wait);
+        digitalWrite(audioPin03, HIGH);
+    }    
+    if (cmd == "04") {      
+        digitalWrite(audioPin04, LOW);
+        lowPin = "04";
+        delay(wait);
+        digitalWrite(audioPin04, HIGH);
+    }    
+    if (cmd == "05") {     
+        digitalWrite(audioPin05, LOW);
+        lowPin = "05";
+        delay(wait);
+        digitalWrite(audioPin05, HIGH);
+    }    
+    if (cmd == "06") {       
+        digitalWrite(audioPin06, LOW);
+        lowPin = "06";
+        delay(wait);
+        digitalWrite(audioPin06, HIGH);
+    }    
+    if (cmd == "07") {        
+        digitalWrite(audioPin07, LOW);
+        lowPin = "07";
+        delay(wait);
+        digitalWrite(audioPin07, HIGH);
+    }    
+    if (cmd == "08") {         
+        digitalWrite(audioPin08, LOW);
+        lowPin = "08";
+        delay(wait);
+        digitalWrite(audioPin08, HIGH);
+    }    
+    if (cmd == "09") {         
+        digitalWrite(audioPin09, LOW);
+        lowPin = "09";
+        delay(wait);
+        digitalWrite(audioPin09, HIGH);
+    }    
+    if (cmd == "10") {         
+        digitalWrite(audioPin10, LOW);
+        lowPin = "10";
+        delay(wait);
+        digitalWrite(audioPin10, HIGH);
+    }
 return 1;   // Returns 1 so we know all went well.. 
 }
 
 
 void loop(){                    // The loop that runs all the time.. looper = 0 reserved
-    if (looper == 1){           // onBasic; just lights. Engine static, cockpit blinking, running lights external. Not headlights.
-        onBasic();              // Since the cockpitlights should flicker this must loop.. 
-    }
-    if (looper == 2){           // onAdvanced; lights and sound
-        onAdvanced();
-    }
+    connect();
+
     if (looper == 3){           // takeOff, some work done here. Not done.
         takeOff();
     }
-    if (looper == 4){           // hyperdrive; haven't done anything here yet 
-    }
+
     if (looper == 5){           // The annoying sequence used for testing.. 
         leftrightleft();        // Development only, this triggers the leftrightleftright audio file
         flashybit();            // Silly light sequence
     }
 }
 
+
+// Display Functions
 void leftrightleft(){               // Voice going left right left, but I got mono so left left left left...annoying!
-    digitalWrite(audioPin00, LOW);  // Pin triggered
+    pinResetFast(audioPin00);       // Pin triggered
 }
 
 void flashybit(){
@@ -189,11 +260,6 @@ void flashybit(){
   }
 }
 
-void pinFixer(){ // Remember to add the pins here that are used for audio..
-    if (prevCmd == "takeOff") digitalWrite(audioPin02, HIGH);
-    if (prevCmd == "annoying") digitalWrite(audioPin00, HIGH); 
-}
-
 void onBasic(){
     colorEngine(0,0,255);           // The initial blue
 }
@@ -206,7 +272,11 @@ void takeOff (){                            // Very early attempt at a take off 
     unsigned long currentMillis = millis();
     if (audio == 0){                        // I just want the audio pin to trigger ONCE..so I have to create some sort of controller. audio was set to 0 during power up
         digitalWrite(audioPin02, LOW);      // Trigger audio
+        lowPin = "02";
         audio = 1;                          // Set audio to 1 as the sound has been triggered..no need to do it again
+        if (currentMillis - sequenceStart >1000){
+            digitalWrite(audioPin02, HIGH);
+        }
     }
     if (audio == 1){                        // Since audio has started, we need to start the lights
         if (currentMillis - sequenceStart <= 5780){     // The MF engine lights are darker blue for a bit, then as the engines "explode" to life, they turn a different color..the audio file is about 5610 ms in when this happens..
@@ -237,40 +307,60 @@ void externalLights(){
     // Again, just a placeholder for future use..this time the external lighting. 
 }
 
+
+// Management Functions
+void pinFixer(String lowPin){ // Remember to add the pins here that are used for audio..
+    if (lowPin == "00") pinSetFast(audioPin00);
+    if (lowPin == "01") pinSetFast(audioPin01);
+    if (lowPin == "02") pinSetFast(audioPin02);
+    if (lowPin == "03") pinSetFast(audioPin03);
+    if (lowPin == "04") pinSetFast(audioPin04);
+    if (lowPin == "05") pinSetFast(audioPin05);
+    if (lowPin == "06") pinSetFast(audioPin06);
+    if (lowPin == "07") pinSetFast(audioPin07);
+    if (lowPin == "08") pinSetFast(audioPin08);
+    if (lowPin == "09") pinSetFast(audioPin09);
+    if (lowPin == "10") pinSetFast(audioPin10);
+}
+
 void volumeU(int v){                            // Volume up function
-    for(int y=0; y<v; y++) {
-        digitalWrite(volumeUp, LOW);            // Basically this sets the volume up pin low for 5 ms, then sets it high again
+    for(int y=0; y<v; y++) {          // Basically this sets the volume up pin low for 5 ms, then sets it high again
+        pinResetFast(volumeUp);
         delay(5);
-        digitalWrite(volumeUp, HIGH);
+        pinSetFast(volumeUp);
     }
 }
 
 void volumeD(int v){                            // Volume down function
     for(int y=v; y>0; y--) {
-        digitalWrite(volumeDown, LOW);          // Basically this sets the volume down pin low for 5 ms, then sets it high again
+        pinResetFast(volumeDown);        // Basically this sets the volume down pin low for 5 ms, then sets it high again
         delay(5);
-        digitalWrite(volumeDown, HIGH);
+        pinSetFast(volumeDown);
     }
 }
-
 
 void lightsOff(){                               // Function to clear the lights..having issues with this as well, and that indicates issues with the library..
     pixels.clear();                           // Initialize pins for output
     pixels.show();                        // Turn all LEDs off ASAP, this does not allways work for some reason
-
-
 }
 
 void relay(String cmd){                 // The relay that controls the power to the amp takes a string command, on or off
     if (cmd == "off"){                   // Reason for this is to remove static hum when displaying only lights
-        digitalWrite(relPin, LOW);
+        pinResetFast(relPin);
     }else if(cmd == "on"){
-        digitalWrite(relPin, HIGH);
+        pinSetFast(relPin);
     }
 }
 
+
 void resetAudio(){                  // Trips the rst pin on the audio fx board. Used to stop playing of files mid-flight
-    digitalWrite(audioRst, LOW);
-    delay(1000);
-    digitalWrite(audioRst, HIGH);
+    pinResetFast(audioRst);
+    delay(wait);
+    pinSetFast(audioRst);
+}
+
+void connect() {
+  if (Particle.connected() == false) {  // If not connected, then.. 
+    Particle.connect();                 // connect!
+  }
 }
